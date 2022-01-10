20890a36ef7dfe79d0b910da4321b0cc73ddffd1
OleksandrS <oleksandr.salivon@kanini.com>
2022-01-10 10:55:41 +0200

    Linear layout example

--- app/src/main/java/com/example/shop_list/domain/ShopItem.kt
+++ app/src/main/java/com/example/shop_list/domain/ShopItem.kt
@@ -3,7 +3,7 @@ package com.example.shop_list.domain
 data class ShopItem(
     val name: String,
     val count: Int,
-    val visibility: Boolean,
+    val enable: Boolean,
     var id : Int = UNDEFINED_ID,
 ) {
 
